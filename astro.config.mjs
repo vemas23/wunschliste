028e3df4b8330045ea8c9820cbb08b6dc34ef39a
Paul <paul.brysch@gmx.net>
2025-02-12 17:08:33 +0100

    add cloudflare intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+ }),
 });
